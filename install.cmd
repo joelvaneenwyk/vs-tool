e67ffcb83d58f7723e48ab60b484d65ffeee68d6
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-01-31 20:59:08 -0800

    Added support for command line options to 'install.cmd' so you could install just the version you want.

--- install.cmd
+++ install.cmd
@@ -6,16 +6,22 @@ net session >nul 2>&1
 		Echo.This batch file needs to be run with administrative privileges. Since it copies files to the \Program Files directory.
 		Pause
 		Goto cleanup
-) 
+)
 
 REM On 64-bit machines, Visual Studio 2013/2015 and MsBuild are in the (x86) directory. So try that last.
 if exist "%ProgramFiles%" set "MsBuildRootDir=%ProgramFiles%\MSBuild\Microsoft.Cpp\v4.0"
 if exist "%ProgramFiles(x86)%" set "MsBuildRootDir=%ProgramFiles(x86)%\MSBuild\Microsoft.Cpp\v4.0"
 
 echo.%MsBuildRootDir%
+
 set /a n=0
+
+if "%1" == "2012" set /a n=1
+if "%1" == "2013" set /a n=2
+if "%1" == "2015" set /a n=3
+
 :loopVisualStudioVersion
-echo.%n%
+
 if %n%==0 (
 	set VsVersion=2010
 	set "MsBuildCppDir=%MsBuildRootDir%\Platforms"
@@ -47,34 +53,35 @@ if %i%==1 set CppVersion=Emscripten
 if %i%==2 set CppVersion=MinGW
 if %i%==3 set CppVersion=NaCl
 if %i%==4 (
-set /a n=%n%+1
-goto loopVisualStudioVersion
+	set /a n=%n%+1
+	if not "%1" == "" goto complete
+	goto loopVisualStudioVersion
 )
 
-if exist %MsBuildCppDir%\%CppVersion% (	
-	Echo."%CppVersion%" Cpp MsBuild toolset already exists.
-	Echo.Continuing will delete the version already installed to this directory: 
+if exist "%MsBuildCppDir%\%CppVersion%\Microsoft.Cpp.%CppVersion%.props" (
+	Echo."%CppVersion%" Cpp MsBuild toolset already exists
+	Echo.Continuing will delete the version already installed to this directory
+	Pause
 
-	rd "%MsBuildCppDir%\%CppVersion%" /s		
-	if exist "%MsBuildCppDir%\%CppVersion%" (
-		Echo.Failed to REMove directory
-		Pause
+	rmdir "%MsBuildCppDir%\%CppVersion%" /s /q
+	if exist "%MsBuildCppDir%\%CppVersion%\Microsoft.Cpp.%CppVersion%.props" (
+		Echo.Failed to remove directory
 		goto cleanup
 	)
 	echo.
 )
 
-md "%MsBuildCppDir%\%CppVersion%"
+if not exist "%MsBuildCppDir%\%CppVersion%" mkdir "%MsBuildCppDir%\%CppVersion%"
 
 echo.Installing %CppVersion% MSBuild files:
 cd /d %~dp0
 xcopy "%CppVersion%\*.*" "%MsBuildCppDir%\%CppVersion%" /E
 if %CppVersion%==Emscripten xcopy "%VsVersion% DLL\*.dll" "%MsBuildCppDir%\%CppVersion%" /E
 
-if errorlevel 1 (	
+if errorlevel 1 (
 	echo.Problem with copying
 	Pause
-	goto cleanup	
+	goto cleanup
 )
 
 SET /a i=%i%+1
