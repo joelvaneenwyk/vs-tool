1b1237dbce231864e727d37a2b8e33713964528d
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-12-30 12:02:12 -0800

    Merge vs-android project.

---
+++ MSBuild/vs-android.Build.CPPTasks.Android/AdbDeploy.cs
@@ -0,0 +1,254 @@
+﻿// ***********************************************************************************************
+// (c) 2012 Gavin Pugh http://www.gavpugh.com/ - Released under the open-source zlib license
+// ***********************************************************************************************
+
+// Apache Ant, Apk Building Task.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Xml;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.CPPTasks;
+using Microsoft.Build.Utilities;
+
+namespace vs_android.Build.CPPTasks.Android
+{
+	public class AdbDeploy : TrackedVCToolTask
+	{
+		public bool BuildingInIDE { get; set; }
+
+		[Required]
+		public string AntBuildPath { get; set; }
+
+		[Required]
+		public string AntBuildType { get; set; }
+
+		[Required]
+		public string AdbPath { get; set; }
+
+		[Required]
+		public string Params { get; set; }
+
+		[Required]
+		public string DeviceArgs { get; set; }
+
+		public string GenerateCmdFilePath { get; set; }
+
+		private AntBuildParser m_parser = new AntBuildParser();
+
+		private string m_toolFileName;
+
+		public AdbDeploy()
+			: base(new ResourceManager("vs_android.Build.CppTasks.Android.Properties.Resources", Assembly.GetExecutingAssembly()))
+		{
+
+		}
+
+		private void WriteDebugRunCmdFile()
+		{
+			string destCmdFile = Path.GetFullPath(GenerateCmdFilePath);
+
+			using (StreamWriter outfile = new StreamWriter(destCmdFile))
+			{
+				outfile.Write(string.Format("{0} {1} shell am start -n {2}/{3}\n", AdbPath, MakeStringReplacements(DeviceArgs), m_parser.PackageName, m_parser.ActivityName));
+			}
+		}
+
+		protected override bool ValidateParameters()
+		{
+			m_toolFileName = Path.GetFileNameWithoutExtension(ToolName);
+
+			if ( !m_parser.Parse( AntBuildPath, AntBuildType, Log, false ) )
+			{
+				return false;
+			}
+
+			return base.ValidateParameters();
+		}
+
+		public override void Cancel()
+		{
+			Process.Start(AdbPath, "kill-server");
+
+			base.Cancel();
+		}
+
+		public override bool Execute()
+		{
+			return base.Execute();
+		}
+
+		protected override int ExecuteTool(string pathToTool, string responseFileCommands, string commandLineCommands)
+		{
+			Log.LogMessage(MessageImportance.High, "{0} {1}", pathToTool, commandLineCommands);
+
+			if ( ( GenerateCmdFilePath != null ) && ( GenerateCmdFilePath.Length > 0 ) )
+			{
+				WriteDebugRunCmdFile();
+			}
+
+			if ( commandLineCommands.Contains( "wait-for-device" ) || commandLineCommands.Contains( "start-server" ) )
+			{
+				// Hack to spawn a process, instead of waiting on it
+				Process.Start( pathToTool, commandLineCommands );
+				return 0;
+			}
+			else
+			{
+				return base.ExecuteTool( pathToTool, responseFileCommands, commandLineCommands );
+			}
+		}
+
+		public override bool AttributeFileTracking
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		protected override string GetWorkingDirectory()
+		{
+			return AntBuildPath;
+		}
+
+		private string MakeStringReplacements( string theString )
+		{
+			string paramCopy = theString;
+			paramCopy = paramCopy.Replace("{PackageName}", m_parser.PackageName);
+			paramCopy = paramCopy.Replace("{ApkPath}", "\"" + m_parser.OutputFile + "\"");
+			paramCopy = paramCopy.Replace("{ActivityName}", m_parser.ActivityName);
+			return paramCopy.Trim();
+		}
+
+		protected override string GenerateCommandLineCommands()
+		{
+			return (MakeStringReplacements(DeviceArgs) + " " + MakeStringReplacements(Params)).Trim();
+		}
+
+#if !VS2010DLL && !VS2015DLL && !VS2017DLL
+		protected override string GenerateResponseFileCommands(VCToolTask.CommandLineFormat format)
+		{
+			return string.Empty;
+		}
+#endif
+
+		protected override string GenerateResponseFileCommands()
+		{
+			return string.Empty;
+		}
+
+		protected override bool MaintainCompositeRootingMarkers
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public virtual string PlatformToolset
+		{
+			get
+			{
+				return "Adb";
+			}
+		}
+
+		protected override Encoding ResponseFileEncoding
+		{
+			get
+			{
+				return Encoding.ASCII;
+			}
+		}
+
+		protected override string ToolName
+		{
+			get
+			{
+				return AdbPath;
+			}
+		}
+
+		protected override ITaskItem[] TrackedInputFiles
+		{
+			get
+			{
+				return new TaskItem[] { new TaskItem( m_parser.OutputFile) };
+			}
+		}
+
+		protected override string TrackerIntermediateDirectory
+		{
+			get
+			{
+				if (this.TrackerLogDirectory != null)
+				{
+					return this.TrackerLogDirectory;
+				}
+				return string.Empty;
+			}
+		}
+
+		public virtual string TrackerLogDirectory
+		{
+			get
+			{
+				if (base.IsPropertySet("TrackerLogDirectory"))
+				{
+					return base.ActiveToolSwitches["TrackerLogDirectory"].Value;
+				}
+				return null;
+			}
+			set
+			{
+				base.ActiveToolSwitches.Remove("TrackerLogDirectory");
+				ToolSwitch switch2 = new ToolSwitch(ToolSwitchType.Directory)
+				{
+					DisplayName = "Tracker Log Directory",
+					Description = "Tracker log directory.",
+					ArgumentRelationList = new ArrayList(),
+					Value = VCToolTask.EnsureTrailingSlash(value)
+				};
+				base.ActiveToolSwitches.Add("TrackerLogDirectory", switch2);
+				base.AddActiveSwitchToolValue(switch2);
+			}
+		}
+
+		protected override string CommandTLogName
+		{
+			get
+			{
+				return (m_toolFileName + ".command.1.tlog");
+			}
+		}
+
+		protected override string[] ReadTLogNames
+		{
+			get
+			{
+				return new string[] { (m_toolFileName + ".read.*.tlog"), (m_toolFileName + ".*.read.*.tlog") };
+			}
+		}
+
+		protected override string[] WriteTLogNames
+		{
+			get
+			{
+				return new string[] { (m_toolFileName + ".write.*.tlog"), (m_toolFileName + ".*.write.*.tlog") };
+			}
+		}
+
+	}
+
+
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+vs-tool
+=======
+
+vs-tool is a Visual Studio plugin to integrate external compiler/linker toolchains to the VS IDE. It supports Visual Studio 2010, 2012, 2013, and 2015.
+
+This plugin is a derivation of the excellent work done by Gavin Pugh for the <a href="http://code.google.com/p/vs-android/">vs-android</a> tool, and is therefore also licensed under the <a href="http://en.wikipedia.org/wiki/Zlib_License">Zlib license</a>.
+
+Features
+--------
+- Seamlessly integrates MinGW (GCC), Clang and Emscripten toolchains to Visual Studio.
+- Appears as a new Solution Platform to the Visual Studio projects. Does not destroy or interfere with existing Visual Studio functionality.
+- Does not require setting up new solution/project files to target, can be added to existing solutions.
+- Adapts the Project Properties dialog to show compiler/linker parameters specific to each tool.
+
+Important! At the current stage, the vs-tool plugin should be considered experimental and hackish in nature. This means that when you update to a newer version of the plugin, solutions that use vs-tool CAN LOSE previously set configuration in the solution/project property pages, or even completely FAIL to load into Visual Studio. You were warned.
+
+If you want to help with the project, please contribute with bug reports and patches.
+
+Plugin Installation
+-------------------
+
+For each <i>platform</i> you are interested in using (Clang, Emscripten, MinGW and/or NaCl), do the following:
+
+1. Copy the folder <i>platform</i>\ from this repository to C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\Platforms\\<i>platform</i>\ (or the corresponding location where MSBuild exists on your system)
+2. To enable an existing solution to be built via vs-tool, create a a platform for it from Configuration Manager -> Active Solution Platform -> New... -> <i>platform</i>.
+3. To choose the toolchain to build with, edit the dropdown list at Project Properties -> General -> Platform Toolset.
+
+Setup for MinGW
+---------------
+MinGW is a toolchain to compile native Windows applications using a Windows port of the GCC compiler. With vs-tool you can build Visual Studio solutions using MinGW.
+
+1. Install MinGW from http://www.mingw.org/
+2. Set MINGW_BIN environment variable to point to the bin\ directory where the MinGW toolchain executables (gcc.exe et al.) reside in.
+3. Alternatively, in Visual Studio, go to Project Properties -> Toolchain Directories -> MinGW Compiler Path, and specify there the bin\ directory where the MinGW toolchain executables are located.
+
+Setup for Clang
+---------------
+<a href="http://clang.llvm.org/">Clang</a> is a C/C++ frontend for the <a href="http://llvm.org/">LLVM Compiler Infrastructure</a>. Vs-tool can also invoke Clang to build Visual Studio solutions. Note however, that Clang does not yet support building native Windows applications.
+
+1. Obtain Clang. For example, follow this web page for instruction how to manually build Clang: http://clang.llvm.org/get_started.html .
+2. Set CLANG_BIN environment variable to point to the directory where the Clang toolchain executables (clang.exe and the rest) reside in. 
+3. Alternatively, in Visual Studio, go to Project Properties -> Toolchain Directories -> Clang Compiler Path, and specify there the directory where the Clang toolchain executables are located.
+
+Setup for Emscripten (emcc)
+--------------
+Emscripten is a compiler/linker that allows you to compile C/C++ code to JavaScript. See http://emscripten.org/ . Vs-tool can be used to build Visual Studio solutions to JavaScript.
+
+1. Follow these instructions to set up Emscripten: https://github.com/kripken/emscripten/wiki/Tutorial .
+2. Emscripten requires Clang to be set up. Follow the above instructions to set up Clang for vs-tool.
+4. Set EMSCRIPTEN environment variable to point to the emscripten root directory (location of emcc.bat et al) 
+5. Alternatively, in Visual Studio, go to Project Properties -> Toolchain Directories -> Emcc Linker Path, and specify there the directory to the emscripten root directory.
