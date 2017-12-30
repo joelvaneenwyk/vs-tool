ffa99334c5047d5f8744b81bd08368008e30b899
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-12-30 15:12:13 -0800

    Fixed build.cmd to compile correctly for VS2010. Updated DLLs for all but VS2012.

--- build.cmd
+++ build.cmd
@@ -9,6 +9,7 @@ call :BuildProject vs2010
 goto :eof
 
 :BuildProject
+SETLOCAL ENABLEDELAYEDEXPANSION
 REM set VERBOSE=verbosity:diagnostic
 
 if "%1" == "vs2017" (
@@ -29,21 +30,31 @@ if "%1" == "vs2017" (
 	set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio %VS%\Common7\IDE\devenv.com"
 )
 
+set SLN=%~dp0\workspace\vs-tool.%1.sln
+
 if "%1" == "vs2010" (
-	set CONFIG="Release"
+	set PRJ=vs-tool.Build.CPPTasks.%1
+	set OPTIONS=!SLN! %VERBOSE% /Build Release /Project !PRJ!
 ) else (
-	set CONFIG="Release"
+	set PRJ=%~dp0\vs-tool.Build.CPPTasks\vs-tool.Build.CPPTasks.%1.csproj
+	set OPTIONS=!SLN! %VERBOSE% /Build Release /Project !PRJ!
 )
+
+echo.
+echo ===========================================================================
+echo.
 
 if exist %DEVENV% (
 	echo Compiling %1 [%VS%]
-
-	set SLN=%~dp0/workspace/%1/vs-android.Build.CPPTasks.Android.sln
-	set PRJ=%~dp0/workspace/%1/vs-android.Build.CPPTasks.Android.csproj
-
-	%DEVENV% %SLN% %VERBOSE% /Build %CONFIG% /Project %PRJ%
+	echo %DEVENV% %OPTIONS%
+	%DEVENV% %OPTIONS%
 ) else (
 	echo Can't find compiler for %1
 )
 
+echo.
+echo ===========================================================================
+echo.
+
+ENDLOCAL
 goto:eof
