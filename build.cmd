9063ab459e2c0ff6ddb2cece7a7674257dcb1e5e
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-12-29 22:58:42 -0800

    Added build script for compiling with every version (only working for VS2017, VS2015, and VS2010 currently)

---
+++ build.cmd
@@ -0,0 +1,43 @@
+@echo off
+
+call :BuildProject vs2017
+call :BuildProject vs2015
+call :BuildProject vs2013
+call :BuildProject vs2012
+call :BuildProject vs2010
+
+goto :eof
+
+:BuildProject
+REM set VERBOSE=verbosity:diagnostic
+
+if "%1" == "vs2017" (
+	set VS=15.0
+) else if "%1" == "vs2015" (
+	set VS=14.0
+) else if "%1" == "vs2013" (
+	set VS=12.0
+) else if "%1" == "vs2012" (
+	set VS=11.0
+) else if "%1" == "vs2010" (
+	set VS=10.0
+)
+
+if "%1" == "vs2017" (
+	set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.com"
+) else (
+	set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio %VS%\Common7\IDE\devenv.com"
+)
+
+if exist %DEVENV% (
+	echo Compiling %1 [%VS%]
+
+	set SLN=%~dp0/workspace/%1/vs-android.Build.CPPTasks.Android.sln
+	set PRJ=%~dp0/workspace/%1/vs-android.Build.CPPTasks.Android.csproj
+
+	%DEVENV% %SLN% %VERBOSE% /Rebuild Release /Project %PRJ%
+) else (
+	echo Can't find compiler for %1
+)
+
+goto:eof
--- workspace/vs2017/Properties/Resources.Designer.cs
+++ workspace/vs2017/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace vs_android.Build.CPPTasks.Android {
+namespace vs_android.Build.CPPTasks.Android.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace vs_android.Build.CPPTasks.Android {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("vs_android.Build.CPPTasks.Android.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("vs_android.Build.CPPTasks.Android.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
