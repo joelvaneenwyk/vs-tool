0b589b58d300f5bcfd00833fa6943bb4d6ee9e13
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2017-12-30 18:27:23 -0800

    Fixed WIN32 version of sample.

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -1,13 +1,18 @@
-#include <emscripten.h>
 #include <string>
 #include <cstdio>
 
+#ifndef _WIN32
+#include <emscripten.h>
+#endif
+
 void printMessage(const char *message)
 {
+#ifndef _WIN32
     // Alternative that uses JavaScript
     char buffer[2048];
     snprintf(buffer, 2048, "console.log('Using console.log: %s');", message);
     emscripten_run_script(buffer);
+#endif
 
     // The newline is actually necessary to force it to flush
     printf("%s\n", message);
@@ -16,5 +21,5 @@ void printMessage(const char *message)
 int main()
 {
     printMessage("Hello from emscripten world!");
-	return 1;
+    return 1;
 }
