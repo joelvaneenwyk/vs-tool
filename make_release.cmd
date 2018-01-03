89ef9fdefc8c00d6c0bdb0c4d0a95e1b2ca172f2
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2018-01-02 18:32:15 -0800

    Fixed release script.

--- make_release.cmd
+++ make_release.cmd
@@ -1,3 +1,5 @@
 @echo off
-del release.zip
-"C:\Program Files\7-Zip\7z.exe" a release.zip license.txt readme.txt MSBuild\*DLL MSBuild\install_* MSBuild\Android\*
+set RELEASE_ZIP=vs-tool-release.zip
+set FILES=license.txt readme.txt Bin\10.0\vs-tool*.dll Bin\11.0\vs-tool*.dll Bin\12.0\vs-tool*.dll Bin\14.0\vs-tool*.dll Bin\15.0\vs-tool*.dll MSBuild\Android\* MSBuild\Clang\* MSBuild\MinGW\* MSBuild\NaCl\*
+if exist %RELEASE_ZIP% del %RELEASE_ZIP%
+"C:\Program Files\7-Zip\7z.exe" a %RELEASE_ZIP% %FILES%
