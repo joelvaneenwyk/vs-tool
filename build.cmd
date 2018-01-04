7fc61cb5b46c321cb4a59afecbbfe8a7f7491e21
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2018-01-04 11:47:35 -0800

    Added much more comprehensive testing to build script for DevEnv and MSBuild.

--- build.cmd
+++ build.cmd
@@ -9,60 +9,98 @@ call :BuildProject vs2010
 goto :eof
 
 :BuildProject
-SETLOCAL ENABLEDELAYEDEXPANSION
-REM set VERBOSE=verbosity:diagnostic
+    SETLOCAL ENABLEDELAYEDEXPANSION
+    REM set VERBOSE=/verbosity:diagnostic
 
-if "%1" == "vs2017" (
-	set VS=15.0
-) else if "%1" == "vs2015" (
-	set VS=14.0
-) else if "%1" == "vs2013" (
-	set VS=12.0
-) else if "%1" == "vs2012" (
-	set VS=11.0
-) else if "%1" == "vs2010" (
-	set VS=10.0
-)
+    if "%1" == "vs2017" (
+        set VS=15.0
+        set TOOLS_VERSION=!VS!
+        set DEFINES=VS2017DLL
+    ) else if "%1" == "vs2015" (
+        set VS=14.0
+        set TOOLS_VERSION=!VS!
+        set DEFINES=VS2015DLL
+    ) else if "%1" == "vs2013" (
+        set VS=12.0
+        set TOOLS_VERSION=!VS!
+        set DEFINES=VS2013DLL
+    ) else if "%1" == "vs2012" (
+        set TOOLS_VERSION=4.0
+        set VS=11.0
+        set DEFINES=VS2012DLL
+    ) else if "%1" == "vs2010" (
+        set TOOLS_VERSION=4.0
+        set VS=10.0
+        set DEFINES=VS2010DLL
+    )
 
-if "%1" == "vs2017" (
-	set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.com"
-) else (
-	set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio %VS%\Common7\IDE\devenv.com"
-)
+    if "%1" == "vs2017" (
+        set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.com"
+    ) else (
+        set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio %VS%\Common7\IDE\devenv.com"
+    )
 
-set SLN=%~dp0\Workspace\vs-tool.%1.sln
+    set MSBUILD="c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\MSBuild.exe"
 
-if "%1" == "vs2010" (
-	set PRJ1=vs-tool.Build.CPPTasks.%1
-	set PRJ2=simple.%1
-) else (
-	set PRJ1=%~dp0\Source\vs-tool.Build.CPPTasks\vs-tool.Build.CPPTasks.%1.csproj
-	set PRJ2=%~dp0\Samples\simple\simple.%1.vcxproj
-)
+    set SLN=%~dp0\Workspace\vs-tool.%1.sln
 
-set OPTIONS_VSTOOL=!SLN! %VERBOSE% /Rebuild Release /Project !PRJ1!
-set OPTIONS_EMSCRIPTEN=!SLN! %VERBOSE% /Rebuild "Release|Emscripten" /Project !PRJ2!
+    set PROJECT_VSTOOL=%~dp0\Source\vs-tool.Build.CPPTasks\vs-tool.Build.CPPTasks.%1.csproj
+    set PROJECT_SAMPLE_SIMPLE=%~dp0\Samples\simple\simple.%1.vcxproj
 
-echo ===========================================================================-----
-echo Compiling %1 [%VS%]
-echo ===========================================================================-----
+    set DEVENV_PROJECT_VSTOOL=%~dp0\Source\vs-tool.Build.CPPTasks\vs-tool.Build.CPPTasks.%1.csproj
+    set DEVENV_PROJECT_SAMPLE_SIMPLE=%~dp0\Samples\simple\simple.%1.vcxproj
 
-if exist %DEVENV% (
-	echo %DEVENV% %OPTIONS_VSTOOL%
-	%DEVENV% %OPTIONS_VSTOOL%
+    if "%1" == "vs2010" (
+        set DEVENV_PROJECT_VSTOOL=vs-tool.Build.CPPTasks.%1
+        set DEVENV_PROJECT_SAMPLE_SIMPLE=simple.%1
+    )
 
-	echo.
-	echo ===========================================================================-----
+    echo ===========================================================================-----
+    echo Compiling %1 [%VS%]
+    echo ===========================================================================-----
 
-	echo %DEVENV% %OPTIONS_EMSCRIPTEN%
-	%DEVENV% %OPTIONS_EMSCRIPTEN%
-) else (
-	echo Can't find compiler for %1
-)
+    if not exist !DEVENV! (
+        goto:eof
+    )
 
-echo.
-echo ===========================================================================-----
-echo.
+    endlocal & (
+        set _MSBUILD_VSTOOL=%MSBUILD% %PROJECT_VSTOOL% %VERBOSE% /property:Configuration=Release /p:Platform="AnyCPU" /p:DefineConstants="%DEFINES%" /t:Clean,Build /tv:%TOOLS_VERSION%
+        set _MSBUILD_SAMPLE_SIMPLE=%MSBUILD% %PROJECT_SAMPLE_SIMPLE% %VERBOSE% /property:Configuration=Release /p:Platform="Emscripten" /t:Clean,Build /tv:%TOOLS_VERSION%
 
-ENDLOCAL
-goto:eof
+        set _DEVENV_VSTOOL=%DEVENV% %SLN% %VERBOSE% /Rebuild Release /Project %DEVENV_PROJECT_VSTOOL%
+        set _DEVENV_SAMPLE_SIMPLE=%DEVENV% %SLN% %VERBOSE% /Rebuild "Release|Emscripten" /Project %DEVENV_PROJECT_SAMPLE_SIMPLE%
+    )
+
+    call :SET_EMSCRIPTEN_DIR D:\SDKs\emsdk\emscripten\1.37.26
+
+    echo %_MSBUILD_VSTOOL%
+    %_MSBUILD_VSTOOL%
+    echo.
+    echo ===========================================================================-----
+
+    echo %_MSBUILD_SAMPLE_SIMPLE%
+    %_MSBUILD_SAMPLE_SIMPLE%
+    echo.
+    echo ===========================================================================-----
+
+    echo %_DEVENV_VSTOOL%
+    %_DEVENV_VSTOOL%
+    echo.
+    echo ===========================================================================-----
+
+    echo %_DEVENV_SAMPLE_SIMPLE%
+    %_DEVENV_SAMPLE_SIMPLE%
+
+    echo.
+    echo ===========================================================================-----
+    echo.
+    goto:eof
+
+:SET_EMSCRIPTEN_DIR
+	if exist %1\emcc.bat set EMSCRIPTEN=%~dpfn1
+	if exist %EMSCRIPTEN%\emcc.bat (
+		set ERRORLEVEL=0
+	) else (
+		set ERRORLEVEL=1
+	)
+    goto:eof
