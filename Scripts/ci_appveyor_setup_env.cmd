0af2c1c97a64451e8bbccfc22bfcbc21b166faf1
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2018-01-04 14:03:00 -0800

    Another attempt to fix by not using EMSCRIPTEN_ROOT which seems non-existent.

--- Scripts/ci_appveyor_setup_env.cmd
+++ Scripts/ci_appveyor_setup_env.cmd
@@ -3,6 +3,7 @@ echo Setting up environment for [%PLATFORM%]...
 
 if "%PLATFORM%" == "Emscripten" (
   call %EMSDK%\emsdk_env.bat --global
-  set EMSCRIPTEN=%EMSCRIPTEN_ROOT%
-  echo Emscripten: [%EMSCRIPTEN%]
 )
+
+echo Platform: [%PLATFORM%]
+echo Emscripten: [%EMSCRIPTEN%]
